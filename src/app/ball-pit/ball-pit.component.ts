c692cd09f31277a9eab3e1aa3250b8547d279c44
Jarod Neuner <jarod@neuner.us>
2016-09-17 13:50:05 -0500

    Support for multiple balls.

--- src/app/ball-pit/ball-pit.component.ts
+++ src/app/ball-pit/ball-pit.component.ts
@@ -7,54 +7,85 @@ import 'fabric/dist/fabric';
     <canvas id="c" width="500px" height="500px"></canvas>
   `
 })
+
 export class BallPitComponent implements OnInit {
   ngOnInit() {
     let c = new fabric.StaticCanvas('c');
-
-    let r = new fabric.Circle({
-      left: 0,
-      top: 100,
-      fill: 'red',
-      radius: 40,
-    });
+	let circsA = [
+		ball('red', 40, 0.35, 0.22),
+		ball('blue', 50, 0.12, 0.45),
+		ball('yellow', 25, 0.22, 0.19),
+		ball('green', 35, 0.26, 0.41),
+		ball('orange', 44, 0.28, 0.23),
+		ball('purple', 21, 0.39, 0.31),
+	];
 	
-    c.add(r);
+	circsA.forEach(function (item) { c.add(item.circle); } );
 
 	let docBody = document.getElementsByTagName("body")[0];
 	let canvas = document.getElementById("c");
-	const xVelocity:number = 0.35;
-	const yVelocity:number = 0.22;
 
     function draw(time:number) {
-	  let docStyle = window.getComputedStyle(docBody, null);
-	  let docWidth = parseInt(docStyle.getPropertyValue("width"));
-	  let docHeight = parseInt(docStyle.getPropertyValue("height")) 
-			- parseInt(docStyle.getPropertyValue("margin-top"));
-	  //console.log(docStyle);
+		let docStyle = window.getComputedStyle(docBody, null);
+		let docWidth = parseInt(docStyle.getPropertyValue("width"));
+		let docHeight = parseInt(docStyle.getPropertyValue("height")) 
+				- parseInt(docStyle.getPropertyValue("margin-top"));
 	  
-	  canvas.style.height = docHeight + "px";
-	  c.setHeight(docHeight);
-	  canvas.style.width = docWidth + "px";
-	  c.setWidth(docWidth);
+		canvas.style.height = docHeight + "px";
+		c.setHeight(docHeight);
+		canvas.style.width = docWidth + "px";
+		c.setWidth(docWidth);
 		
-	  let diameter:number = r.radius << 1;
-	  let width:number = docWidth - diameter;
-	  let height:number = docHeight - diameter;
-	  let xPos:number = (time * xVelocity) % width;
-	  let yPos:number = (time * yVelocity) % height;
-	  
-      if((((time * xVelocity) / width) % 2.0) >= 1.0) {
-		xPos = width - xPos;
-      }
-      if((((time * yVelocity) / height) % 2.0) >= 1.0) {
-		yPos = height - yPos;
-      }
-
-      r.set('left', xPos);
-	  r.set('top', yPos);
-
-      c.renderAll();
+		circsA.forEach(function(item) {item.draw(time, docWidth, docHeight); });
+		
+		c.renderAll();
     }
+	
+	function ball(bc:string, br:number, xV:number, yV:number) {
+		if (bc == undefined) {
+			bc = 'red';
+		}
+		if (br == undefined) {
+			br = 40;
+		}
+		if (xV == undefined) {
+			xV = 0.2;
+		}
+		if (yV == undefined) {
+			yV = 0.2;
+		}
+		
+		var ballObj = {
+			color : bc,
+			radius : br,
+			xVelocity : xV,
+			yVelocity : yV,
+				
+			circle : new fabric.Circle({
+				fill: bc,
+				radius: br,
+			}),
+		
+			draw : function(time:number, docWidth:number, docHeight:number) {
+				let diameter:number = this.circle.radius << 1;
+				let width:number = docWidth - diameter;
+				let height:number = docHeight - diameter;
+				let xPos:number = (time * this.xVelocity) % width;
+				let yPos:number = (time * this.yVelocity) % height;
+				  
+				if((((time * this.xVelocity) / width) % 2.0) >= 1.0) {
+					xPos = width - xPos;
+				}
+				if((((time * this.yVelocity) / height) % 2.0) >= 1.0) {
+					yPos = height - yPos;
+				}
+
+				this.circle.set('left', xPos);
+				this.circle.set('top', yPos);
+			}
+		}
+		return ballObj;
+	}
 
     function animationLoop(time:number) {
       fabric.util.requestAnimFrame(animationLoop);
